05a13aba955eface156270ae3142a0407788e0e6
Thibeau Fuhrer <thf@studer-raimann.ch>
2021-02-11 16:42:30 +0100

    increased plugin and ilias-max version

--- plugin.php
+++ plugin.php
@@ -1,16 +1,16 @@
 <?php
- 
+
 // alphanumerical ID of the plugin; never change this
 $id = "xmma";
- 
+
 // code version; must be changed for all code changes
-$version = "1.2.0";
- 
+$version = "1.2.1";
+
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.4.0";
-$ilias_max_version = "5.4.999";
- 
+$ilias_min_version = "5.2.0";
+$ilias_max_version = "6.9.999";
+
 // optional, but useful: Add one or more responsible persons and a contact email
-$responsible = "studer + raimann ag - Core 1";
-$responsible_mail = "support-core1@studer-raimann.ch";
+$responsible = "Stefan Born";
+$responsible_mail = "stefan.born@phzh.ch";
